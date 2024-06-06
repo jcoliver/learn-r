b16f66665d4bbdfb9723b71777c1e162b8521b2e
Jeff Oliver <jcoliver@arizona.edu>
2024-06-06 10:55:09 -0700

    Add more comments to mice clustering script

--- dev-scripts/mice-clustering-imputation.R
+++ dev-scripts/mice-clustering-imputation.R
@@ -5,27 +5,37 @@
 
 library(mice)
 
-# General idea:
-# Use mice to impute missing values, creating some number of datasets (10, 100)
-# Run clustering analysis on each dataset
-# Summarize results...somehow
-# Lowest hanging fruit:
-# Calculate distance matrix for each imputed dataset
-# Average all those distance matrices ("consensus" matrix)
-# Run hclust on that matrix for "consensus" cluster
+# Approach described below:
+#   + Impute multiple datasets
+#   + Calculate distance matrix for each imputed dataset (with dist() function)
+#   + Average all those distance matrices to create a "consensus" matrix
+#   + Run hclust on that matrix for "consensus" cluster
 
-# Start by trying hclust on mtcars
+# Another approach (not implemented, but might be possible with clusterMI 
+# package, https://cran.r-project.org/web/packages/clusterMI/index.html):
+#   + Impute multiple datasets
+#   + Run clustering analysis on each imputed dataset
+#   + Summarize results on all clustering output
+
+################################################################################
+# Start by trying hclust on mtcars (a built-in dataset), just to see how 
+# process would work if there were no missing data
+
 # First calculate Euclidian distance matrix
 mtcars_dist <- dist(x = mtcars)
 # Now do clustering
 mtcars_hclust <- hclust(d = mtcars_dist)
-# Plot to see what that looks like; produces a histogram
+# Plot to see what that looks like; produces a dendrogram
 plot(mtcars_hclust)
+
+################################################################################
+# Now try the imputation approach. Start by creating a dataset that has missing
+# data
 
 # Make a copy of the mtcars dataset to add missing data
 my_cars <- mtcars
 # Add 10 missing points, by randomly picking row & column numbers; fingers 
-# crossed we don't pick same cell twice?
+# crossed we don't pick same cell twice :)
 num_missing <- 10
 row_na <- sample(x = 1:nrow(my_cars), replace = TRUE, size = num_missing)
 col_na <- sample(x = 1:ncol(my_cars), replace = TRUE, size = num_missing)
@@ -40,12 +50,14 @@ cars_imp <- mice(data = my_cars, m = 10)
 # mice::complete gives us a list, each element is an imputed dataset
 cars_data <- mice::complete(cars_imp, action = "all")
 
-# Calculate distance matrix for each imputed dataset
+# Calculate distance matrix for each imputed dataset; output is another list, 
+# each element is a distance matrix
 cars_dist <- lapply(X = cars_data, FUN = dist)
 
-# Now create consensus with all those distance matrices
+# Now create consensus with all those distance matrices; code below takes the 
+# average for each cell across the multiple distance matrices
 consensus_dist <- Reduce(f = "+", x = cars_dist)/length(cars_dist)
 
-# Finally, run hclust for "consensus" cluster
+# Finally, run hclust for consensus cluster on that consensus distance matrix
 consensus_clust <- hclust(d = consensus_dist)
 plot(consensus_clust)
