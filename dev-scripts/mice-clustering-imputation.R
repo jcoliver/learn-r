6594b94c0623f5854d61300815c4cfd0e27769c1
Jeff Oliver <jcoliver@arizona.edu>
2024-06-20 12:20:15 -0700

    Comments on mice clustering script

--- dev-scripts/mice-clustering-imputation.R
+++ dev-scripts/mice-clustering-imputation.R
@@ -30,7 +30,8 @@ plot(mtcars_hclust)
 
 ################################################################################
 # Now try the imputation approach. Start by creating a dataset that has missing
-# data
+# data. A fairly brute-force approach is applied here; the ampute function from 
+# the mice package is an alternative.
 
 # Make a copy of the mtcars dataset to add missing data
 my_cars <- mtcars
@@ -61,3 +62,56 @@ consensus_dist <- Reduce(f = "+", x = cars_dist)/length(cars_dist)
 # Finally, run hclust for consensus cluster on that consensus distance matrix
 consensus_clust <- hclust(d = consensus_dist)
 plot(consensus_clust)
+
+################################################################################
+# Comparisons of mice built-in visualization functions, including:
+# bwplot
+# densityplot
+# plot
+# stripplot
+# xyplot
+
+# Boxplot of observed and imputed data, by imputation number. Observed data is 
+# first box, remaining boxes are imputed values. Appear to *only* show imputed
+# values (much smaller sample sizes)
+bwplot(cars_imp)
+
+# Density plot, comparing to observed
+# densityplot(cars_imp, ~ mpg) # this one errors out?
+# densityplot(cars_imp, ~ wt) # errors
+# densityplot(cars_imp, ~ qsec) # errors
+densityplot(cars_imp, ~ drat)
+densityplot(cars_imp, ~ hp)
+
+# Plot of mean & sd for each imputation iteration. Not so useful.
+plot(cars_imp)
+
+# Plots all variables and indicates imputed values for each imputation 
+# iteration; would like to be able to only show those variables that were 
+# imputed...
+stripplot(cars_imp)
+
+# Pairwise comparison between two variables. Points for all iterations are 
+# shown; blue for observed, red for imputed values
+xyplot(cars_imp, hp ~ drat)
+
+################################################################################
+# + t-tests comparing observed vs. imputed distributions 
+# + boxplots comparing observed vs. imputed distributions 
+# + [stretch] scatterplots of pairwise variable comparisons with error bars for 
+#   imputed points
+
+# 
+# 1. For the visual distribution, how can I do a box plot (with all 52 
+#    variables, or only those with missing values)?
+# 2. For the ggplot of distribution, how can I use two colors to differentiate 
+#    the two types of variables (original and imputed)?
+# 3. For the visual comparison between two variables, now I use the 
+#    "consensus_data", so there is only one dot for one imputed value, because 
+#    that is the mean value of 10 imputed values. But in our meeting, we say we 
+#    probably can have 10 little dots for one imputed value, because there are 
+#    10 imputed datasets, if not use the mean of them. How can we do that to 
+#    consider variation as well?
+# 4. How to put error bars on the imputed values in the plot of variable-based 
+#    comparison, if possible?
+
