8f445f1a4ca0d5d250ed8717e096298f32d7d075
Jeff Oliver <jcoliver@email.arizona.edu>
2024-06-21 14:30:48 -0700

    Update comments in mice clustering script

--- dev-scripts/mice-clustering-imputation.R
+++ dev-scripts/mice-clustering-imputation.R
@@ -71,13 +71,19 @@ plot(consensus_clust)
 # stripplot
 # xyplot
 
+# Before running code below, be sure sample cars imputed data is in memory 
+# (i.e. make sure to run library(mice) and lines 37 - 48).
+
 # Boxplot of observed and imputed data, by imputation number. Observed data is 
 # first box, remaining boxes are imputed values. Appear to *only* show imputed
 # values (much smaller sample sizes)
+# Plots all variables, whether or not values were imputed
 bwplot(cars_imp)
+# Explicitly call out variables of interest (in this case, mpg, disp, hp & wt)
+bwplot(cars_imp, mpg + disp + hp + wt ~ .imp)
 
-# Density plot, comparing to observed
-# densityplot(cars_imp, ~ mpg) # this one errors out?
+# Density plot, comparing to observed, not very useful.
+# densityplot(cars_imp, ~ mpg) # this one errors out
 # densityplot(cars_imp, ~ wt) # errors
 # densityplot(cars_imp, ~ qsec) # errors
 densityplot(cars_imp, ~ drat)
@@ -86,32 +92,13 @@ densityplot(cars_imp, ~ hp)
 # Plot of mean & sd for each imputation iteration. Not so useful.
 plot(cars_imp)
 
-# Plots all variables and indicates imputed values for each imputation 
-# iteration; would like to be able to only show those variables that were 
-# imputed...
-stripplot(cars_imp)
+# Plots variables and indicates imputed values for each imputation iteration. 
+# Line below creates plots for every variable, individually
+stripplot(cars_imp, mpg + disp + hp + drat + wt + vs + am + carb ~ .imp)
+# Create pairwise plot of two variables (in this case, mpg ~ disp)
+stripplot(cars_imp, mpg ~ disp)
 
 # Pairwise comparison between two variables. Points for all iterations are 
-# shown; blue for observed, red for imputed values
-xyplot(cars_imp, hp ~ drat)
-
-################################################################################
-# + t-tests comparing observed vs. imputed distributions 
-# + boxplots comparing observed vs. imputed distributions 
-# + [stretch] scatterplots of pairwise variable comparisons with error bars for 
-#   imputed points
-
-# 
-# 1. For the visual distribution, how can I do a box plot (with all 52 
-#    variables, or only those with missing values)?
-# 2. For the ggplot of distribution, how can I use two colors to differentiate 
-#    the two types of variables (original and imputed)?
-# 3. For the visual comparison between two variables, now I use the 
-#    "consensus_data", so there is only one dot for one imputed value, because 
-#    that is the mean value of 10 imputed values. But in our meeting, we say we 
-#    probably can have 10 little dots for one imputed value, because there are 
-#    10 imputed datasets, if not use the mean of them. How can we do that to 
-#    consider variation as well?
-# 4. How to put error bars on the imputed values in the plot of variable-based 
-#    comparison, if possible?
-
+# shown; blue for observed, red for imputed values. Very similar to the output 
+# of stripplot for pairwise plot (above), but xyplot looks better.
+xyplot(cars_imp, mpg ~ disp)
